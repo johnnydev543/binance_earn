51473ba7c87038af0322c11864f75a901de5a028
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2021-06-01 18:24:26 +0800

    use get_asset_balance

--- script.py
+++ script.py
@@ -26,79 +26,74 @@ TARGET_LOT = config['target']['LOT']
 # at least buy X amount of lot in single purchase
 MIN_LOT = int(config['target']['MIN_LOT'])
 
-client = Client(API_KEY, API_SECRET)
+client_api = Client(API_KEY, API_SECRET)
 
 # lending endpoint is not implemented in the package,
 # so we need to force it to use the url and version we want
+client = Client(API_KEY, API_SECRET)
 client.API_URL = 'https://api.binance.com/sapi'
 client.PRIVATE_API_VERSION = "v1"
 
 while(True):
 
-    params = {
-        'timestamp': time.time()
-    }
+    asset_balance = client_api.get_asset_balance(asset=TARGET_COIN)
 
-    capital = client._get("capital/config/getall",
-                            True, client.PUBLIC_API_VERSION, data=params)
+    free = asset_balance.get('free', None)
+    free_balance = float(free)
 
-    for coin in capital:
-        free = coin.get('free', None)
-        coin = coin.get('coin', None)
-        free_balance = float(free)
-        if free_balance > 0 and coin == TARGET_COIN :
-            print(TARGET_COIN, free_balance)
+    if free_balance > 0:
+        print(TARGET_COIN, free_balance)
 
-            params = {
-                'type': 'CUSTOMIZED_FIXED',
-                'timestamp': time.time()
-            }
+        params = {
+            'type': 'CUSTOMIZED_FIXED',
+            'timestamp': time.time()
+        }
 
-            projects = client._get("lending/project/list",
-                                    True, client.PUBLIC_API_VERSION, data=params)
-
-            for project in projects:
-                status = project.get('status', None)
-                asset = project.get('asset', None)
-                duration = project.get('duration', None)
-                lotSize = project.get('lotSize', None)
-
-                if asset == TARGET_COIN and status == 'PURCHASING' and int(duration) == TARGET_DURATION:
-                    lotsPurchased = project.get('lotsPurchased', None)
-                    lotsUpLimit = project.get('lotsUpLimit', None)
-                    maxLotsPerUser = project.get('maxLotsPerUser', None)
-                    projectId = project.get('projectId', None)
-
-                    print(duration, lotsPurchased, lotsUpLimit, maxLotsPerUser, projectId)
-
-                    purchase_availability = lotsUpLimit - lotsPurchased
-                    balance_lot = free_balance / int(lotSize)
-                    balance_lot = math.floor(balance_lot)
-                    print(datetime.now(), 'purchase_availability', purchase_availability)
-                    # if purchase_availability > balance_lot:
-
-
-                    if balance_lot < MIN_LOT:
-                        continue
-
-                    if purchase_availability > balance_lot:
-
-                        if TARGET_LOT == 'MAX':
-                            lot = balance_lot
-                        else:
-                            lot = int(TARGET_LOT)
-                    else:
-                        lot = purchase_availability
-
-                    params = {
-                        'projectId': projectId,
-                        'lot': lot,
-                        'timestamp': time.time()
-                    }
-
-                    print("Purchase it!")
-                    purchase = client._post("lending/customizedFixed/purchase",
+        projects = client._get("lending/project/list",
                                 True, client.PUBLIC_API_VERSION, data=params)
+
+        for project in projects:
+            status = project.get('status', None)
+            asset = project.get('asset', None)
+            duration = project.get('duration', None)
+            lotSize = project.get('lotSize', None)
+
+            if asset == TARGET_COIN and status == 'PURCHASING' and int(duration) == TARGET_DURATION:
+                lotsPurchased = project.get('lotsPurchased', None)
+                lotsUpLimit = project.get('lotsUpLimit', None)
+                maxLotsPerUser = project.get('maxLotsPerUser', None)
+                projectId = project.get('projectId', None)
+
+                print(duration, lotsPurchased, lotsUpLimit, maxLotsPerUser, projectId)
+
+                purchase_availability = lotsUpLimit - lotsPurchased
+                balance_lot = free_balance / int(lotSize)
+                balance_lot = math.floor(balance_lot)
+                print(datetime.now(), 'purchase_availability', purchase_availability)
+                # if purchase_availability > balance_lot:
+
+
+                if balance_lot < MIN_LOT:
+                    continue
+
+                if purchase_availability > balance_lot:
+
+                    if TARGET_LOT == 'MAX':
+                        lot = balance_lot
+                    else:
+                        lot = int(TARGET_LOT)
+                else:
+                    lot = purchase_availability
+
+                params = {
+                    'projectId': projectId,
+                    'lot': lot,
+                    'timestamp': time.time()
+                }
+
+                print("Purchase it!")
+                purchase = client._post("lending/customizedFixed/purchase",
+                            True, client.PUBLIC_API_VERSION, data=params)
     time.sleep(LOOP_SEC)
 
 
