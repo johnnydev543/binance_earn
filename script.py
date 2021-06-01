2587438bb6f0641ac1d31f410dd3da2e006220b4
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2021-06-01 11:45:39 +0800

    keep working

--- script.py
+++ script.py
@@ -15,6 +15,8 @@ except:
 
 API_KEY = config['api']['API_KEY']
 API_SECRET = config['api']['API_SECRET']
+TARGET_COIN = config['target']['COIN']
+TARGET_DURATION = config['target']['DURATION']
 
 client = Client(API_KEY, API_SECRET)
 
@@ -30,35 +32,32 @@ params = {
 capital = client._get("capital/config/getall",
                         True, client.PUBLIC_API_VERSION, data=params)
 
-TARGET_COIN = 'BUSD'
-TARGET_DURATION = 90
-
 for coin in capital:
     free = coin.get('free', None)
     coin = coin.get('coin', None)
     free_balance = float(free)
     if free_balance > 0 and coin == TARGET_COIN :
-        print(free_balance)
+        print(TARGET_COIN, free_balance)
 
-# print(capital)
+        params = {
+            'type': 'CUSTOMIZED_FIXED',
+            'timestamp': time.time()
+        }
 
-params = {
-    'type': 'CUSTOMIZED_FIXED',
-    'timestamp': time.time()
-}
-projects = client._get("lending/project/list",
-                        True, client.PUBLIC_API_VERSION, data=params)
+        projects = client._get("lending/project/list",
+                                True, client.PUBLIC_API_VERSION, data=params)
 
+        for project in projects:
+            status = project.get('status', None)
+            asset = project.get('asset', None)
+            duration = project.get('duration', None)
 
-for project in projects:
-    status = project.get('status', None)
-    asset = project.get('asset', None)
-    duration = project.get('duration', None)
-    if asset == TARGET_COIN and status == 'PURCHASING' and duration == TARGET_DURATION:
-        lotsPurchased = project.get('lotsPurchased', None)
-        lotsUpLimit = project.get('lotsUpLimit', None)
-        maxLotsPerUser = project.get('maxLotsPerUser', None)
-        print(duration, lotsPurchased, lotsUpLimit, maxLotsPerUser)
-        purchase_availability = lotsUpLimit - lotsPurchased
-        print('purchase_availability', purchase_availability)
+            if asset == TARGET_COIN and status == 'PURCHASING' and int(duration) == int(TARGET_DURATION):
+                lotsPurchased = project.get('lotsPurchased', None)
+                lotsUpLimit = project.get('lotsUpLimit', None)
+                maxLotsPerUser = project.get('maxLotsPerUser', None)
+                projectId = project.get('projectId', None)
+                print(duration, lotsPurchased, lotsUpLimit, maxLotsPerUser, projectId)
+                purchase_availability = lotsUpLimit - lotsPurchased
+                print('purchase_availability', purchase_availability)
 
